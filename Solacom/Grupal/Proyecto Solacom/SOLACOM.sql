503644e54f168ebacc0e520c35079f4b4a7f7f0c
Daniel Duque <148986240+Danielduque22@users.noreply.github.com>
2024-10-08 10:17:41 -0500

    Add files via upload

--- Solacom/Grupal/Proyecto Solacom/SOLACOM.sql
+++ Solacom/Grupal/Proyecto Solacom/SOLACOM.sql
@@ -53,6 +53,25 @@ IdProvedorFK varchar(25) not null,
 IdClienteFK varchar(25)not null,
 IdServicioFK varchar(25) not null
 );
+
+create table CompraProvedor(
+idCompraP varchar(25) primary key not null,
+Nombreproducto varchar(50) not null,
+HarmonizedCode int not null,
+PrecioProductos float not null,
+CantidadProductos int
+);
+alter table CompraProvedor
+add column IdProvedorFK varchar(50);
+
+alter table CompraProvedor
+add constraint FKProvedorProducto
+foreign key (IdProvedorFK)
+references Provedor(IdProvedor);
+
+alter table GenerarVenta
+drop column TotalVenta;
+
 describe GenerarVenta;
 describe Provedor;
 describe Detalle_compra;
@@ -106,6 +125,13 @@ alter table Registro_Solacom
 add constraint FKServicioSolacom
 foreign key (IdServicioFK)
 references Servicio(IdServicio);
+
+alter table GenerarVenta
+add constraint FKCompraProvedor
+foreign key (NombreProductoFK)
+references CompraProvedor(IdCompraP);
+
+describe GenerarVenta;
 
 DELIMITER //
 create procedure RegistrarCliente(Nombre varchar(50),IdCliente varchar(50),DireccionCliente varchar(100),correo varchar(100))
@@ -248,10 +274,69 @@ insert into Servicio values ('SC0024','Servicios de Ciberseguridad','DSE172','SC
 ('RP0045','Repuestos y Pedidos','DSE190','RP');
 
 select * from Servicio;
-/*
+
+use solacom;
+describe provedor;
+
+alter table provedor
+drop column Nombreproducto;
+
+alter table provedor
+drop column CantidadProductos;
+
+alter table provedor
+drop column PrecioProductos;
+
+alter table provedor
+drop column HarmonizedCode;
+
+alter table provedor
+drop column idCompraP;
+
+insert into Provedor values('1','Pallets'),('2','Dell position boxes'),('3','Dell Mobile position'),('4','Cisco Box Labeled'),('5','Solacom Box Labeled');
+select * from provedor;
+describe CompraProvedor;
+
+alter table CompraProvedor
+modify HarmonizedCode varchar(25);
+
+select * from CompraProvedor;
+
+insert into CompraProvedor values('1','Position Audio Controller II w/ Jack Box','P-PAC II',15.00,25,'1'),('2','Dell 23.8 Monitor @ AML','C2213340-5 SR1','180.00',20,'1'),('3','Speaker USB powered, 3.5mm Audio','C2213213-3 SR1','51.00',15,'1'),('4','Cyberpower 900VA 120V UPS','C2213150-1 SR1','138.95',25,'1'),('5','Network laser color printer','C2213811-2 SR1','269.00',20,'1');
+insert into CompraProvedor values('6','Dell T5820 Win10 IOT 2019 LTSC','C2213794-8 3YS+','7000',15,'2'),('7','Guardian Intelligent WorkstationUserCard','C2214210-1 SR2','80.00',25,'2'),('8','Solacom Mouse Pad','C2213660-1 SR1','3.67',20,'2');
+insert into CompraProvedor values('9','Precision 7780 XCTO BASE','C2213452-11 5YS','30.00',15,'3'),('10','Hardened case black','C2213162-1 SR1','66.99',25,'3'),('11','Optical Mouse','C2213164-1 SR1','14.39',20,'3'),('12','Guardian Intelligent WorkstationUserCard','C2214210-1 SR2','80.00',15,'3'),
+('13','Solacom Mouse Pad','C2213660-1 SR1','3.67',25,'3'),('14','Mono Headsets (w/o adapter)','C2213236-2 SR1','27.19',20,'3'),('15','DA80 USB Hdst adapter w volume','C2213876-1 SR1','18.55',15,'3'),('16','HASP key for stand-alone recording w/w','B3211264-1 SR1','23.00',25,'3'),('17','Jackbox 4 Assembly Rev A','C3210053-1 SR1','18.99',20,'3'),
+('18','RJ45 to RJ45 CABLE 10','C2213217-1 SR1','10.39',15,'3'),('19','USB Programmable 24 key  Keypad','C2213176-1 SR1','159.55',25,'3'),('20','10ft USB 2.0 A Male to A Female cable','C2213735-1 SR1','6.29',20,'3'),('21','VT4000 Docking Station for Laptop','C2213911-1 SR1','35.00',15,'3'),('22','USB keyboard & mouse combo ','C2213367-1 SR1','33.98',25,'3');
+insert into CompraProvedor values('23','Cisco C9200L 24','C2213895-1 SR1','1738.35',20,4),('24','Nema 5-15 to C13 AC Cord (3)','B3210506-1 0036','8.80',15,4),('25','Cisco console cable - RJ45m - DB9F 6ft','C2213800-1 SR1','6.50',25,4),('26','Cat 6A Shielded Grey Ether Cable (12IN)','C2213692-1 0012','161.98',20,4);
+insert into CompraProvedor values('27','HASP key for stand-alone recording w/w','B3211264-1 SR1','23.00',15,5),('28','Perle IOLAN STS4-D','C2213490-1 SR1','545.28',25,5),('29','CAT5e unshielded ethernet cable','C2213201-1 0012','143.99',20,5),('30','DB9 to DB9 Patch Panel','B2213424-2 SR1','11.19',15,5),
+('31','Perle IOLAN RJ45 to DB9 Null modem','C3210065-1 SR1','4.95',25,5),('32','Cat 6A Shielded Blue Ether Cable (60IN)','C3210015-1 0060','35.99',20,5),('33','1U Rackmount shelf (silver)','B2213412-1 SR1','45.49',15,5),('34','PTS Handset w/ 15ft cord','B5100005','6.90',25,5),
+('35','10ft USB 2.0 A Male to A Female cable','C2213735-1 SR1','10.39',20,5),('36','USB Programmable 24 key  Keypad','C2213176-1 SR1','169.55',15,5),('37','Jackbox 4 Assembly Rev A','C3210053-1 SR1','18.99',25,5),('38','RJ45 to RJ45 CABLE 10','C2213217-1 SR1','10.39',20,5);
+
+insert into Registro_Solacom values('5','NSE003','AC0001'),('5','DSE118','AC0002'),('5','DSE119','AC0003'),('5','DSE120','AC0004'),('5','DSE121','AC0005'),
+('5','DSE122','AC0006'),('5','DSE123','AC0007'),('5','DSE124','AC0008'),('5','DSE125','AC0009'),('5','DSE126','AC0010'),
+('5','DSE127','AC0011'),('5','DSE128','AC0012'),('5','DSE129','AC0013'),('5','DSE130','AC0014'),('5','DSE131','AC0015'),
+('5','DSE132','AC0016'),('5','DSE184','AC0017'),('5','DSE185','AC0018'),('5','DSE186','AC0019'),('4','QL002','RP0001');
+insert into Registro_Solacom values('4','DS005','RP0002'),('3','NSE009','RP0003'),('1','DSE98','RP0004'),('1','DSE99','RP0005'),('1','DSE107','RP0006'),
+('1','DSE108','RP0007'),('4','DSE109','RP0008'),('5','DSE110','RP0009'),('5','DSE111','RP0010'),('3','DSE112','RP0011'),
+('3','DSE113','RP0012'),('3','DSE114','RP0013'),('2','DSE115','RP0014'),('2','DSE116','RP0015'),('2','DSE117','RP0016'),
+('2','DSE146','RP0017'),('2','DSE147','RP0018'),('1','DSE148','RP0019'),('3','DSE149','RP0020'),('4','DSE150','RP0021');
+insert into Registro_Solacom values('4','DSE151','RP0022'),('4','DSE152','RP0023'),('5','DSE153','RP0024'),('5','DSE154','RP0025'),('3','DSE155','RP0026'),
+('2','DSE156','RP0027'),('1','DSE157','RP0028'),('1','DSE158','RP0029'),('2','DSE159','RP0030'),('3','DSE160','RP0031'),
+('3','DSE161','RP0032'),('4','DSE162','RP0033'),('1','DSE163','RP0034'),('1','DSE164','RP0035'),('2','DSE165','RP0036'),
+('2','DSE166','RP0037'),('3','DSE167','RP0038'),('3','DSE168','RP0039'),('4','DSE169','RP0040'),('4','DSE170','RP0041');
+insert into Registro_Solacom values('4','DSE187','RP0042'),('2','DSE188','RP0043'),('2','DSE189','RP0044'),('3','DSE190','RP0045'),('1','NT001','SC0001'),
+('5','CSC004','SC0002'),('5','DSE100','SC0003'),('5','DSE101','SC0004'),('1','DSE102','SC0005'),('1','DSE103','SC0006'),
+('1','DSE104','SC0007'),('5','DSE105','SC0008'),('5','DSE106','SC0009'),('1','DSE133','SC0010'),('1','DSE134','SC0011'),
+('1','DSE135','SC0012'),('5','DSE136','SC0013'),('1','DSE137','SC0014'),('5','DSE138','SC0015'),('1','DSE139','SC0016');
+insert into Registro_Solacom values('1','DSE140','SC0017'),('1','DSE141','SC0018'),('1','DSE142','SC0019'),('1','DSE143','SC0020'),('5','DSE144','SC0021'),
+('5','DSE145','SC0022'),('5','DSE171','SC0023'),('5','DSE172','SC0024'),('5','DSE173','SC0025'),('1','DSE174','SC0026'),
+('1','DSE175','SC0027'),('5','DSE176','SC0028'),('5','DSE177','SC0029'),('1','DSE178','SC0030'),('1','DSE179','SC0031'),
+('5','DSE180','SC0032'),('5','DSE181','SC0033'),('1','DSE182','SC0034'),('1','DSE183','SC0035');
+
+describe registro_solacom;
+select * from Registro_Solacom;
 SELECT C.DireccionCliente AS NombreCliente, P.NombreProvedor AS NombreProveedor, G.TotalVenta AS TotalCompra
 FROM GenerarVenta G
 JOIN Cliente C ON G.IdClienteFK = C.IdCliente
 JOIN Provedor P ON G.NombreProductoFK = P.IdProvedor
 WHERE G.FechaVenta = '';
-*/
